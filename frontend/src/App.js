b958176ca22b1bf5997bb16f47e62f6e8c2a4e4a
adzik strymerenciusz <20097873@mail.wit.ie>
2025-01-31 09:43:26 +0000

    Initialised React and made Home Page design

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import HomeScreen from "./components/HomeScreen";
+import React from "react";
+
+function App() {
+  return <HomeScreen />;
+    
+}
+
+export default App;
---
+++ frontend/src/components/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { Upload, Camera } from "lucide-react";
+import "./HomeScreen.css";
+import img from "../assets/background.jpg"
+
+const Home = () => {
+  return (
+    <div style={{
+      backgroundImage: `url(${img})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      width: "100vw",
+      height: "100vh"
+    }}>
+      <header className="header">Facial Emotion Analyser</header>
+      <div className="content">
+        <Button
+          variant="container" 
+          className="upload-btn"
+          startIcon={<Upload size={16} />} 
+        >
+          Upload Image
+        </Button>
+
+        <Button
+          variant="container" 
+          className="camera-btn"
+          startIcon={<Camera size={16} />}
+        >
+          Use Camera
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
